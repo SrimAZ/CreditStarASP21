8224152ba4fd49aeb53325faec26ffa4fe31ae7d
SrimAZ <srim@email.arizona.edu>
2021-12-04 19:56:51 -0800

    Updated Back-end code

--- CreditStarASP21/loginPage.aspx.vb
+++ CreditStarASP21/loginPage.aspx.vb
@@ -3,7 +3,7 @@ Imports Oracle.ManagedDataAccess.Client
 Imports System.Threading
 Imports System.Data.OleDb
 Imports System.Windows.Forms
-
+Imports System.Windows
 
 Public Class WebForm5
     Inherits System.Web.UI.Page
@@ -14,31 +14,31 @@ Public Class WebForm5
 
     Protected Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
         Try
-            Dim oradb As String = "TNS_ADMIN=C:\Users\91809\Oracle\network\admin;USER ID=MIS531GROUP03;PASSWORD=<Enter here>;DATA SOURCE=128.196.27.219:1521/ORCL;PERSIST SECURITY INFO=True"
+            Dim oradb As String = "TNS_ADMIN=C:\Users\91809\Oracle\network\admin;USER ID=MIS531GROUP03;PASSWORD=Q0U.I3OT1;DATA SOURCE=128.196.27.219:1521/ORCL;PERSIST SECURITY INFO=True"
             Dim conn As New OracleConnection(oradb)
             Dim cmd As New OracleCommand()
             Dim reader As OracleDataReader
             conn.Open()
             cmd.Connection = conn
 
-            Dim ip_email As String = UserEmailLogin.Text
-            Dim ip_pwd As String = PwdInput.Text
-            Dim v_email As String = ""
+            Dim ip_empID As String = EmployeeID.Text
+            Dim ip_pwd As String = EmployeePassword.Text
+            Dim v_empid As String = ""
             Dim v_pwd As String = ""
 
-            Dim sql1 As String = "select userid from user_login_details where userid = '" + ip_email + "'"
+            Dim sql1 As String = "select LOGIN_EMPID from LOGIN_DETAILS where userid = '" + ip_empID + "'"
             cmd = New OracleCommand(sql1, conn)
             cmd.CommandType = CommandType.Text
             If cmd.ExecuteReader() Is Nothing Then
                 Console.WriteLine("User Not Found")
-                v_email = ""
+                v_empid = ""
             Else
                 reader = cmd.ExecuteReader()
                 reader.Read()
-                v_email = reader.GetString(0)
+                v_empid = reader.GetString(0)
             End If
 
-            Dim sql2 As String = "select userpwd from user_login_details where userid = '" + ip_email + "'"
+            Dim sql2 As String = "select EMP_PASSWORD from LOGIN_DETAILS where LOGIN_EMPID = '" + ip_empID + "'"
             cmd = New OracleCommand(sql2, conn)
             cmd.CommandType = CommandType.Text
             If cmd.ExecuteReader() Is Nothing Then
@@ -50,12 +50,13 @@ Public Class WebForm5
                 v_pwd = reader.GetString(0)
             End If
 
-            Dim sql3 As String = "select userid from users where useremail = '" + ip_email + "'"
+            Dim sql3 As String = "select LOGIN_EMPID from LOGIN_DETAILS where userid = '" + ip_empID + "'"
             cmd = New OracleCommand(sql3, conn)
             cmd.CommandType = CommandType.Text
             If cmd.ExecuteReader() Is Nothing Then
                 Console.WriteLine("User Not Found")
                 v_pwd = ""
+
                 MessageBox.Show("User does not exist. Please sign up.")
                 Response.Redirect("SignUpPage.aspx")
             Else
@@ -64,7 +65,7 @@ Public Class WebForm5
                 Session("UIDLI") = reader.GetString(0)
             End If
 
-            Dim sql4 As String = "select usergroupid from users where useremail = '" + ip_email + "'"
+            Dim sql4 As String = "select LOGIN_EMPID from LOGIN_DETAILS where userid = '" + ip_empID + "'"
             cmd = New OracleCommand(sql4, conn)
             cmd.CommandType = CommandType.Text
             reader = cmd.ExecuteReader()
@@ -74,7 +75,7 @@ Public Class WebForm5
 
             conn.Close()
 
-            If v_email = ip_email And v_pwd = ip_pwd Then
+            If v_empid = ip_empID And v_pwd = ip_pwd Then
                 MessageBox.Show("User Login Successful")
                 If gid = "UGRP102" Then
                     Response.Redirect("employeeMaster.aspx")
